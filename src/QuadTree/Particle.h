cc4b69591f51945ed79bd484b2defe485a918d40
Jimmy <i.elgamal@tum.de>
2022-12-30 22:10:41 +0100

    Added descriptions to classes

--- src/QuadTree/Particle.h
+++ src/QuadTree/Particle.h
@@ -1,5 +1,6 @@
 #ifndef PARTICLE
 #define PARTICLE
+// class to hold the position and velocity of a particle
 
 #include <sstream>
 #include <vector>
@@ -11,22 +12,26 @@ typedef glm::dvec3 myvec3; // We can easily switch the entire implementation to
                            // float precision by adjusting these two variables
 typedef double myfloat;
 
-struct Particle {
+struct Particle
+{
 public:
   myvec3 p{0, 0, 0};
   myvec3 v{0, 0, 0};
   myfloat m{0};
 };
 
-inline std::ostream &operator<<(std::ostream &out, const Particle &p) {
+inline std::ostream &operator<<(std::ostream &out, const Particle &p)
+{
   // Helper function for the particle class, so we can print and debug it easily
   return out << p.p << "\tv=" << p.v << "\tm=" << p.m;
 }
 
 inline std::ostream &operator<<(std::ostream &out,
-                                const std::vector<Particle> &particles) {
+                                const std::vector<Particle> &particles)
+{
   // Print the resulting values of all particles in a .csv format
-  for (auto &p : particles) {
+  for (auto &p : particles)
+  {
     out << p.p[0] << "," << p.p[1] << "," << p.p[2] << ",";
   }
   return out;
