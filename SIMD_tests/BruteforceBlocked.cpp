d50d27c95eb723952f1a5553b0946329a55fcb76
Philipp Jurasic <philipp.jurasic@tum.de>
2024-02-14 23:21:48 +0100

    Forces in separate file

--- SIMD_tests/BruteforceBlocked.cpp
+++ SIMD_tests/BruteforceBlocked.cpp
@@ -1,41 +1,9 @@
 #include <math.h>
 #include <iostream>
 #include <omp.h>
+#include "Forces.h"
 
 const size_t block = 100ul;
-
-inline double length2(double x, double y, double z) {
-  return x * x + y * y + z * z;
-}
-
-void bruteForceAcc(double  *accx, double  *accy, double   *accz,
-                  const double  *xin, const double  *yin, const double  *zin,
-                  const double  x, const double  y, const double  z,
-                  const double  *m, const size_t n) {
-  /* Tried manual vectorization using simd accumulate here, no measurable
-   * difference at -Ofast -march=native. GLM already uses vectorized simd
-   * instructions for the accumulation.
-   */
-  double dvx = 0, dvy = 0, dvz = 0;
-  #pragma omp simd
-  for (size_t i = 0; i < n; i++)
-  {
-    double diffx = xin[i] - x;
-    double diffy = yin[i] - y;
-    double diffz = zin[i] - z;
-
-    constexpr double softening_param = 0.025;
-    auto r2 = length2(diffx, diffy, diffz)+softening_param;
-    double mOverDist3 = m[i] / (r2 * std::sqrt(r2));
-    
-    dvx += diffx * mOverDist3;
-    dvy += diffy * mOverDist3;
-    dvz += diffz * mOverDist3;
-  }
-  *accx = dvx;
-  *accy = dvy;
-  *accz = dvz;
-}
 
 void stepSimulation(double *x, double *y, double *z, 
                     double *vx, double *vy, double *vz, 
@@ -105,5 +73,4 @@ int main() {
     std::cout<<"."<<std::flush;
   }
   std::cout << "x: "<<x[0]<<", y: "<<y[0]<<", z: "<<z[0]<<", vx: "<<vx[0]<<", vy: "<<vy[0]<<", vz: "<<vz[0]<<", m: "<<m[0]<<"\n";
-
 }
