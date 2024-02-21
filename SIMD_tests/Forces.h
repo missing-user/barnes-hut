1674e203c51c1b6847eefbb48d926af18b09b9be
Philipp Jurasic <philipp.jurasic@tum.de>
2024-02-21 20:00:33 +0100

    removed SIMD experiments

--- SIMD_tests/Forces.h
+++
@@ -1,6 +0,0 @@
-#include <math.h>
-
-void bruteForceAcc(double  *accx, double  *accy, double   *accz,
-                  const double  *xin, const double  *yin, const double  *zin,
-                  const double  x, const double  y, const double  z,
-                  const double  *m, const size_t n);
